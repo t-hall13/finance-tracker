6332a97ca04f229981055d68e23002464667f8c6
Tim Hall <timothypatrickhall@gmail.com>
2016-07-29 04:29:09 +0000

    Add destroy friendship and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,11 @@ class UsersController < ApplicationController
       else
          redirect_to my_friends_path, flash[:error]= "There was an error with adding user as a friend"
       end   
-      
+   end
+   
+   def show
+      @user = User.find(params[:id])
+      @user_stocks = @user.stocks  
    end
    
 end
